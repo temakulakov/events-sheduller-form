569fa5454b11fcb6c5167116661d65d207bce369
Artem Kulakov <109291017+temakulakov@users.noreply.github.com>
2025-03-10 12:32:32 +0300

    Update UserFields.ts

--- src/services/UserFields.ts
+++ src/services/UserFields.ts
@@ -3,7 +3,7 @@
     import {UserField, UserFieldAPI, UserFieldsResponse} from "../types";
 
     const fetchUserFields = async (): Promise<UserField[]> => {
-        const { data } = await axios.get<{ result: UserFieldAPI[] }>('https://intranet.bakhrushinmuseum.ru/rest/3/ynm1gnbjjm2kf4vk/crm.deal.userfield.list');
+        const { data } = await axios.get<{ result: UserFieldAPI[] }>('https://intranet.bakhrushinmuseum.ru/rest/1/e3stpev74xjsn8m2/crm.deal.userfield.list');
         return data.result.map(field => ({
                 id: parseInt(field.ID),
                 title: field.FIELD_NAME,
